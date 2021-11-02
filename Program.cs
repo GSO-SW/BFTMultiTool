a586cba8263ad08f51c29ee7aaa3197267ff48b9
Kander Akinci <k.akinci@bkgl.bgl.schule>
2021-11-02 16:30:38 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFTMultiTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
