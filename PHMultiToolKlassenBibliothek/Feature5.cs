5ca5d6b6f1d6aeb2988e63da482e65bd51614f07
Jonas Högele <jonas.h50@gso.schule.koeln>
2021-11-27 12:47:48 +0100

    SUb Menü erstellt

--- PHMultiToolKlassenBibliothek/Feature5.cs
+++ PHMultiToolKlassenBibliothek/Feature5.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Figgle;
 
 namespace PHMultiToolKlassenBibliothek
 {
@@ -11,7 +12,83 @@ namespace PHMultiToolKlassenBibliothek
         public static void Feature5Funcion1()
         {
             Console.WriteLine("Feature5 SubSubMenue");
-        }
 
+
+            string HauptAusw;
+
+            bool Exit = false;
+            bool Hmenue;
+
+            do
+            {
+                (int, int) cPosBM = Console.GetCursorPosition();
+
+                Console.WriteLine();
+
+                //ASCII art Logo wird erzeugt.
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+
+                //Konsolentitel wird geändert.
+                Console.Title = "BFTMultiTool";
+
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Einheiten umrechnen <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Eingabe: exit\t\t->\tBeendet das Programm");
+                Console.WriteLine("Eingabe: hauptmenü\t->\tZurück zum Hauptmenü");
+
+                //Beschreibung der Software.
+                Console.WriteLine("\n\nHier können Sie Ihre Einheiten schnell umrechnen ohne das Sie große Rechnungen durchführen müssen. Das übernehmen wir für Sie.\n\n");
+
+                Hmenue = false;
+
+                //Eingabeaufforderung 
+                Console.WriteLine("Umrechnung von Einheiten. Wählen Sie aus:\n");
+                Console.WriteLine("\t1 - SI-Einheiten");
+                Console.WriteLine("\t2 - Maßeinheiten");
+                Console.Write("Eingabe:");
+                HauptAusw = Console.ReadLine();
+
+                switch (HauptAusw)
+                {
+
+                    case "1":
+                        Console.Clear();
+                        Volumberechnung.wuerfel();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Volumberechnung.Kugel();
+                        break;
+                    case "hauptmenü":
+                        Console.Clear();
+                        Hmenue = true;
+                        break;
+                    case "exit":
+                        Exit = true;
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Ungültige Eingabe");
+                        Console.ReadKey();
+
+                        (int, int) cPosAM = Console.GetCursorPosition();
+
+
+                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
+
+                        break;
+                }
+
+
+            } while (!Exit & !Hmenue);
+
+            if (Exit)
+                Environment.Exit(0);
+        }
     }
 }
