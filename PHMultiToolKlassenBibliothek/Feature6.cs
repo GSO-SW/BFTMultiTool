82bbdd671180646eed5f666f58dcfd55ac817241
Arad Ahmad <arad.a2@gso.schule.koeln>
2021-12-03 12:42:50 +0100

    änderung 2

--- PHMultiToolKlassenBibliothek/Feature6.cs
+++ PHMultiToolKlassenBibliothek/Feature6.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Figgle;
+
 
 namespace PHMultiToolKlassenBibliothek
 {
@@ -10,18 +12,47 @@ namespace PHMultiToolKlassenBibliothek
     {
         public static void Feature6Funcion1()
         {
+            bool Subexit = false;
             do
             {
-                Console.WriteLine("Schwerkraft");
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Schwerkraft <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
                 Console.WriteLine("Hallo liebe leute, im folgenden Text werdet Ihr erfahren wie Ihr die Schwerkraft auf der Erde und auf dem Mond berechnet könnt\n" +
-                    "Die Gewichtskraft, oder auch Schwerkraft genannt, ist die Kraft auf einen Körper, welche durch die Wirkung einer Gravitation erzeugt wird. Du kannst sie mit dem Produkt aus" +
-                    " Masse m und Erdbeschleunigung g berechnen.\nJe größer dabei die Masse des Körpers ist, umso größer ist auch seine Gewichtskraft.Die Größe der Kraft hängt ebenso vom Ort ab, an " +
-                    "dem sich der betreffende Körper befindet.\nDies wird durch den Ortsfaktor g ausgedrückt, welcher auf der Erdoberfläche der Erdbeschleunigung entspricht.Die Einheit der " +
-                    "Gewichtskraft ist Newton[N].\n\nErde Ortskraft:9,81[m/(s^2)] und Mond Ortskraft 1,62[m/(s^2)]\nDies ist die formel:F_g = m x g\nF_g = Gewichtskraft\nm= Masse\ng= Ortsfaktor");
+                                  "Die Gewichtskraft, oder auch Schwerkraft genannt, ist die Kraft auf einen Körper, welche durch die Wirkung einer Gravitation erzeugt wird.\n" +
+                                  "Du kannst sie mit dem Produkt aus Masse m und Erdbeschleunigung g berechnen. Je größer dabei die Masse des Körpers ist, umso größer ist auch\n" +
+                                  "seine Gewichtskraft.Die Größe der Kraft hängt ebenso vom Ort ab, an dem sich der betreffende Körper befindet.Dies wird durch den Ortsfaktor g ausgedrückt,\n" +
+                                  "welcher auf der Erdoberfläche der Erdbeschleunigung entspricht.Die Einheit der Gewichtskraft ist Newton[N].\n" +
+                                  "\nErde Ortskraft:\t9,81[m/(s^2)] " +
+                                  "\nMond Ortskraft:\t1,62[m/(s^2)]" +
+                                  "\n\nDies ist die formel:\tF_g = m x g" +
+                                  "\n\n\tF_g = Gewichtskraft" +
+                                  "\n\tm= Masse" +
+                                  "\n\tg= Ortsfaktor");
                 Console.WriteLine("\num ins Submenue zu kommen, geben Sie Subexit in die eingabe");
-                String str_Gewicht = Console.ReadLine();
+                string str_Gewicht = Console.ReadLine();
+
+                if(str_Gewicht=="Subexit")
+                {
+                    Subexit = true;
+                }
+                else
+                {
+                    
+                            
+                            
+                            
+                 
+                }
+
+
+                double dou_Gewicht = Convert.ToDouble(str_Gewicht);
+
             }
-            while (true);
+            while (Subexit==false);
         }
 
     }
