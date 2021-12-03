f063a4c70fe73a388907d30b92ac9a1e47415d25
Deniz <kartal.deniiz@yahoo.com>
2021-12-03 12:54:57 +0100

    bearbeite ich zuhause

--- WIMultiToolKlassenBibliothek/Feature15.cs
+++ WIMultiToolKlassenBibliothek/Feature15.cs
@@ -10,8 +10,22 @@ namespace WIMultiToolKlassenBibliothek
     {
         public static void Feature15Funcion1()
         {
+            int num1 = 0; 
             Console.WriteLine("Feature15 SubSubMenue");
-            //hallo 
+            Console.WriteLine("Wilkommen Zur Lohnsteuerberechnung.");  //Begrüßung
+            Console.WriteLine("Bitte Tragen sie ihren Bmgl ein.");     //Nach Bmgl Fragen
+            num1 = Convert.ToInt32(Console.ReadLine());
+            {
+                while (num1 > 0)
+                {
+                    Console.WriteLine($"i: {num1}");
+                    num1++;
+                    if (num1 == 0)
+                    { break;
+                        Console.WriteLine("wow");
+                }
+            }
         }
+    }
     }
 }
