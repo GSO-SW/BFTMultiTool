4903bb9e4fc372d38a296fba25cb951487e18ec8
Samir Yusuf Muric <samiryusu.m@gso.schule.koeln>
2021-12-08 12:24:41 +0100

    Programm fertig geschrieben (wird noch verfeinert)

--- MAMultiToolKlassenBibliothek/Feature19.cs
+++ MAMultiToolKlassenBibliothek/Feature19.cs
@@ -45,16 +45,16 @@ namespace MAMultiToolKlassenBibliothek
 
                
                 Console.WriteLine("Wählen Sie eine der folgenden möglichkeiten zum Flächeninhalt berechnen:\n");
-                Console.WriteLine("\t1 -Flächeninhalt eines Quadratsberechnen ");
-                Console.WriteLine("\t2 -Flächeninhalt eines rechtecks berechnen ");
-                Console.WriteLine("\t3 - Flächeninhalt eines allgemeinem Dreieck berechnen");
-                Console.WriteLine("\t4 - Flächeninhalt eines rechtwinkligem Dreieck berechnen");
-                Console.WriteLine("\t5 - Flächeninhalt eines gleichwinkligem Dreieck berechnen");
-                Console.WriteLine("\t6 - Flächeninhalt eines gleichseitigem Dreieck berechnen");
-                Console.WriteLine("\t7 - Flächeninhalt eines Trapezes berechnen");
-                Console.WriteLine("\t8 - Flächeninhalt eines Parallelogram berechnen");
-                Console.WriteLine("\t9 - Flächeminhalt einer Raute berechnen");
-                Console.WriteLine("\t10 - Flächeninhalt eines Kreises berechnen");
+                Console.WriteLine("\t1 - Flächeninhalt vom Quadrat berechnen ");
+                Console.WriteLine("\t2 - Flächeninhalt vom rechteck berechnen ");
+                Console.WriteLine("\t3 - Flächeninhalt vom allgemeinen Dreieck berechnen");
+                Console.WriteLine("\t4 - Flächeninhalt vom rechtwinkligen Dreieck berechnen");
+                Console.WriteLine("\t5 - Flächeninhalt vom gleichschenkligen Dreieck berechnen");
+                Console.WriteLine("\t6 - Flächeninhalt vom gleichseitigen Dreieck berechnen");
+                Console.WriteLine("\t7 - Flächeninhalt vom Trapez berechnen");
+                Console.WriteLine("\t8 - Flächeninhalt vom Parallelogramm berechnen");
+                Console.WriteLine("\t9 - Flächeminhalt von einer Raute berechnen");
+                Console.WriteLine("\t10 - Flächeninhalt vom Kreis berechnen");
 
 
                 Console.Write("Eingabe:");
@@ -66,7 +66,6 @@ namespace MAMultiToolKlassenBibliothek
                     case "1":
                         Console.Clear();
                         Quadratberechnen();
-
                         break;
 
                     case "2":
@@ -107,7 +106,7 @@ namespace MAMultiToolKlassenBibliothek
 
                     case "8":
                         Console.Clear();
-                        Parallelogram();
+                        Parallelogramm();
 
                         break;
 
@@ -153,53 +152,356 @@ namespace MAMultiToolKlassenBibliothek
 
         static void Quadratberechnen()
         {
-            Console.WriteLine("Quadratberechnen");
+
+            bool wiederholen = true;
+            do
+            {
+
+
+                Double a = 0;
+                Console.WriteLine("Quadrat berechnen");
+                Console.WriteLine("Geben sie den Wert von a ein");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                if (a < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (a > 0)
+                {
+                    Double A = a * a;
+                    Console.WriteLine("Der flächeninhalt des Quadrats beträgt {0}",A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen==true);
+
+                
         }
 
         static void Rechteckberechen()
         {
-            Console.WriteLine("Rechteckberechnen");
+            bool wiederholen = true;
+            do
+            {
+
+
+                Double a = 0, b = 0;
+                Console.WriteLine("Rechteck berechnen");
+                Console.WriteLine("Geben sie den Wert von a ein");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von b ein");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (a < 0 && b < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (a > 0 && b > 0)
+                {
+                    Double A = a * b;
+                    Console.WriteLine("Der flächeninhalt des Rechtecks beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
         static void AllgemeinesDreieck()
         {
-            Console.WriteLine("AllgemeinesDreieck");
+            bool wiederholen = true;
+            do
+            { 
+                Double g = 0, h = 0;
+            Console.WriteLine("Allg. Dreieck berechnen");
+            Console.WriteLine("Geben sie den Wert von g ein");
+            g = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben sie den Wert von h ein");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            if (g < 0 && h < 0)
+            {
+                Console.WriteLine("Falsche Eingabe");
+                Console.ReadLine();
+                wiederholen = true;
+            }
+            else if (g > 0 && h > 0)
+            {
+                Double A = g * h / 2 ;
+                Console.WriteLine("Der flächeninhalt des Allg. Dreiecks beträgt {0}", A);
+                Console.WriteLine("Wiederholen?(true/false)");
+                wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+            }
+
+
+
+
+            Console.Clear();
+
+        } while (wiederholen == true);
         }
 
         static void rechtwinkligesDreieck()
         {
-            Console.WriteLine("rechwinkligesDreieck");
+            bool wiederholen = true;
+            do
+            {
+                Double a = 0, b = 0;
+                Console.WriteLine("Rechtwinkliges Dreieck berechnen");
+                Console.WriteLine("Geben sie den Wert von a ein");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von b ein");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (a < 0 && b < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (a > 0 && b > 0)
+                {
+                    Double A = (a * b) / 2;
+                    Console.WriteLine("Der flächeninhalt des RechtWinkligen Dreiecks beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
         static void gleichwinkligesDreieck()
         {
-            Console.WriteLine("gleichwinkligesDreieck");
+            bool wiederholen = true;
+            do
+            {
+                Double c = 0, h = 0;
+                Console.WriteLine("Gleichschenkliges Dreieck berechnen");
+                Console.WriteLine("Geben sie den Wert von c ein");
+                c = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von h ein");
+                h = Convert.ToDouble(Console.ReadLine());
+
+                if (c < 0 && h < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (c > 0 && h > 0)
+                {
+                    Double A = 0.5 * c * h;
+                    Console.WriteLine("Der flächeninhalt des Rechtwinkligem Dreieck beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
         static void gleichseitigesDreieck()
         {
-            Console.WriteLine("gleichseitigesDreieck");
+            bool wiederholen = true;
+            do
+            {
+                Double a = 0;
+                Console.WriteLine("Gleichseitiges Dreieck  berechnen");
+                Console.WriteLine("Geben sie den Wert von g ein");
+                a = Convert.ToDouble(Console.ReadLine());
+                
+                if (a < 0 )
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (a > 0)
+                {
+                    Double A = ((a * a) /4) * Math.Sqrt(3);
+                    Console.WriteLine("Der flächeninhalt des Gleichseitigem Dreiecks beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
         static void Trapez()
         {
-            Console.WriteLine("Trapez");
+            bool wiederholen = true;
+            do
+            {
+                Double a = 0, c = 0, h = 0;
+                Console.WriteLine("Trapez berechnen");
+                Console.WriteLine("Geben sie den Wert von a ein");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von c ein");
+                c = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von h ein");
+                h = Convert.ToDouble(Console.ReadLine());
+
+                if (a < 0 && c < 0 && h < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (a > 0 && c > 0 && h > 0)
+                {
+                    Double A = (a + c / 2) * h;
+                    Console.WriteLine("Der flächeninhalt des Trapez beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
-        static void Parallelogram()
+        static void Parallelogramm()
         {
-            Console.WriteLine("Parallelogram");
+            bool wiederholen = true;
+            do
+            {
+                Double a = 0, h = 0;
+                Console.WriteLine("Parallelogramm berechnen");
+                Console.WriteLine("Geben sie den Wert von a ein");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von h ein");
+                h = Convert.ToDouble(Console.ReadLine());
+
+                if (a < 0 && h < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (a > 0 && h > 0)
+                {
+                    Double A = a * (h * h) ;
+                    Console.WriteLine("Der flächeninhalt des Parallelogramm beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
         static void Raute()
         {
-            Console.WriteLine("Raute");
+            bool wiederholen = true;
+            do
+            {
+                Double e = 0, f = 0;
+                Console.WriteLine("Raute berechnen");
+                Console.WriteLine("Geben sie den Wert von e ein");
+                e = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben sie den Wert von f ein");
+                f = Convert.ToDouble(Console.ReadLine());
 
+                if (e < 0 && f < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (e > 0 && f > 0)
+                {
+                    Double A = (1 / 2) * e * f;
+                    Console.WriteLine("Der flächeninhalt der Raute beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
 
         static void Kreis()
         {
-            Console.WriteLine("Kreis");
+            bool wiederholen = true;
+            do
+            {
+                Double r = 0;
+                Console.WriteLine("Kreis berechnen");
+                Console.WriteLine("Geben sie den Wert von r ein");
+                r = Convert.ToDouble(Console.ReadLine());
+               
+
+                if (r < 0)
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    Console.ReadLine();
+                    wiederholen = true;
+                }
+                else if (r > 0)
+                {
+                    Double A = 3.14 * (r * r);
+                    Console.WriteLine("Der flächeninhalt des Kreises beträgt {0}", A);
+                    Console.WriteLine("Wiederholen?(true/false)");
+                    wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                }
+
+
+
+
+                Console.Clear();
+
+            } while (wiederholen == true);
         }
     }
 
