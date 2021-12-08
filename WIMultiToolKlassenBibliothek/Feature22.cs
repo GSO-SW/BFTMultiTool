00e0c921a086c0030c4362f829b3952a3e38ac2e
Chahine Djobbi <chahine.d@gso.schule.koeln>
2021-12-08 10:15:38 +0100

    Ich habe Feature22 erstellt und im submenu eingefügt

---
+++ WIMultiToolKlassenBibliothek/Feature22.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIMultiToolKlassenBibliothek
+{
+    class Feature22
+    {
+        public static void Feature22Funcion1()
+        {
+            Console.WriteLine("Feature22 SubSubMenue");
+            Console.Clear();
+        }
+    }
+}
