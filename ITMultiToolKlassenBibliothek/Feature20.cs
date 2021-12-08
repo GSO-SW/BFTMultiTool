f16dd40bdc3145abc0ba1e009ab5342605e89732
Haitham Kazzaz <haitham.k2@gso.schule.koeln>
2021-12-08 10:11:31 +0100

    Feature20 und 21

---
+++ ITMultiToolKlassenBibliothek/Feature20.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMultiToolKlassenBibliothek
+{
+    class Feauture20
+    {
+        public static void Feature10Funcion1()
+        {
+            Console.WriteLine("Feature20 SubSubMenue");
+
+        }
+
+    }
+}
