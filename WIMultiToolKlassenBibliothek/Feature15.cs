9b8e181358d6e5d735be7a16c748b725875c592d
Deniz <kartal.deniiz@yahoo.com>
2021-12-09 19:05:07 +0100

    bin fast fertig nur paar sachen

--- WIMultiToolKlassenBibliothek/Feature15.cs
+++ WIMultiToolKlassenBibliothek/Feature15.cs
@@ -12,69 +12,85 @@ namespace WIMultiToolKlassenBibliothek
     {
         public static void Feature15Funcion1()
         {
-            int num1 = 0;
+            bool wiederholen = true;
+            do
+            {
+                string str_num1;
+                int num1 = 0;
+                bool tryparseout;
 
-            while (num1 > 0)
+
                 Console.WriteLine("Feature15 SubSubMenue");                                                                     //idk war schon da xD
-            Console.WriteLine("Wilkommen Zur Lohnsteuerberechnung.");                                                      //Begrüßung
-            Console.WriteLine("Bitte Tragen sie ihren Bmgl ein.");                                                        //Nach Bmgl Fragen
-            num1 = int.Parse(Console.ReadLine());                                                                        //einsetzen bzw gucken ob das da zu den if dingern passt            
+                Console.WriteLine("Wilkommen Zur Lohnsteuerberechnung.");                                                   //Begrüßung
 
-            if (num1 >= 1 & num1 < 11000)                                                                                           //wenn kleiner als 110000
-            {
-                Console.WriteLine("Sie Haben 0 Euro an lohnsteuern zu zahlen");                                       //den text da ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                      //Bedanken auf ganz süß
-                Console.ReadLine();                                                                                  //Programm schließen
-            }
-            else if (num1 > 11000 & num1 <= 18000)                                                                  //wenn größer als 11000 und kleiner gleich 18000 ist
-            {
+                do
+                {
+                    Console.WriteLine("Bitte Tragen sie ihren Bmgl ein.");                                              //Nach Bmgl Fragen
 
-                Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 25 + "Euro"));                      //ausrechnen und Lohnsteuer ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                     //Bedanken auf ganz süß
-                Console.ReadLine();                                                                            //Programm schließen
-            }
-            else if (num1 == 0)
-            {
-                var prs = new ProcessStartInfo("Chrome.exe");
-                prs.Arguments = "https://youtu.be/dQw4w9WgXcQ";
-                Process.Start(prs);
+                    str_num1 = Console.ReadLine();
 
-            }
-            else if (num1 > 18000 & num1 <= 31000)                                                           //wenn größer als 18000 und kleiner gleich 31000 ist
-            {
-                Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 35 + " Euro"));                    //ausrechnen und Lohnsteuer ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                      //Bedanken auf ganz süß
-                Console.ReadLine();                                                                           //Programm schließen
-            }
-            else if (num1 > 31000 & num1 <= 60000)                                                           //wenn größer als 31000 und kleiner gleich 60000 ist
-            {
-                Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 42 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                   //Bedanken auf ganz süß
-                Console.ReadLine();                                                                            //Programm schließen
-            }
-            else if (num1 > 60000 & num1 <= 90000)                                                           //wenn größer als 60000 und kleiner gleich 90000 ist
-            {
-                Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 48 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                    //Bedanken auf ganz süß
-                Console.ReadLine();                                                                         //Programm schließen
-            }
-            else if (num1 > 90000 & num1 <= 100000)                                                           //wenn größer als 90000 und kleiner gleich 100000 ist 
-            {
+                    tryparseout = int.TryParse(str_num1, out num1);
+                } while (tryparseout==false);                                                           //einsetzen bzw gucken ob das da zu den if dingern passt            
 
-                Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 50 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                    //Bedanken auf ganz süß
-                Console.ReadLine();                                                                         //Programm schließen
-            }
-            else if (num1 > 100000)                                                                           //wenn größer als 100000
-            {
-                Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 55 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
-                Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                    //Bedanken auf ganz süß
-                Console.ReadLine();                                                                         //Programm schließen
+                if (num1 >= 1 & num1 < 11000)                                                                                           //wenn kleiner als 110000
+                {
+                    Console.WriteLine("Sie Haben 0 Euro an lohnsteuern zu zahlen");                                       //den text da ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                      //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                                  //Programm schließen
+                }
+                else if (num1 > 11000 & num1 <= 18000)                                                                  //wenn größer als 11000 und kleiner gleich 18000 ist
+                {
+
+                    Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 25 + "Euro"));                      //ausrechnen und Lohnsteuer ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                     //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                            //Programm schließen
+                }
+                else if (num1 > 18000 & num1 <= 31000)                                                           //wenn größer als 18000 und kleiner gleich 31000 ist
+                {
+                    Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 35 + " Euro"));                    //ausrechnen und Lohnsteuer ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                      //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                           //Programm schließen
+                }
+                else if (num1 > 31000 & num1 <= 60000)                                                           //wenn größer als 31000 und kleiner gleich 60000 ist
+                {
+                    Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 42 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                   //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                            //Programm schließen
+                }
+                else if (num1 > 60000 & num1 <= 90000)                                                           //wenn größer als 60000 und kleiner gleich 90000 ist
+                {
+                    Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 48 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                    //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                         //Programm schließen
+                }
+                else if (num1 > 90000 & num1 <= 100000)                                                           //wenn größer als 90000 und kleiner gleich 100000 ist 
+                {
+
+                    Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 50 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                    //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                         //Programm schließen
+                }
+                else if (num1 > 100000)                                                                           //wenn größer als 100000
+                {
+                    Console.WriteLine($"Ihre Lohnsteuer Beträgt:" + (num1 / 100 * 55 + " Euro"));                  //ausrechnen und Lohnsteuer ausgeben
+                    Console.WriteLine("Wir Bedanken uns das sie unseren Programm verwenden");                    //Bedanken auf ganz süß
+                    Console.ReadLine();                                                                         //Programm schließen
+                }
+                Console.ReadKey();                                                                               //NACH KNOPF DRUCK BEENDEN
+                Console.WriteLine("Wiederholen?(true/false)");                                                  //fragt nach ob du es nochmal verwenden willst
+                wiederholen = Convert.ToBoolean(Console.ReadLine());
+
+                bool.TryParse();
+                Console.Clear();                                                                                    //macht die anderen zeichen da weg
+            } while (wiederholen);
+            
 
 
 
-            }
+
+
         }
     }
 }
+
 
