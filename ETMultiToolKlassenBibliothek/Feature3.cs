dde93bf468dc11fe7b725388e6320d31bafea052
Nico Billig <nico.b37@gso.schule.koeln>
2021-12-10 12:52:09 +0100

    Weitere Informationen hinzugefügt.

--- ETMultiToolKlassenBibliothek/Feature3.cs
+++ ETMultiToolKlassenBibliothek/Feature3.cs
@@ -117,8 +117,10 @@ namespace ETMultiToolKlassenBibliothek
                 switch (oAuswahl)
                 {
 
-                    case "1":   //Möglichkeit 1
+                    case "1":   //Möglichkeit 1 (Definition und Bedeutung)
                         Console.Clear();
+
+                        Console.Write("Ohmsches Gesetz --- Definition und Bedeutung\n");
 
                         Console.Write("Das ohmsche Gesetz besagt: Die Stärke des durch ein Objekt fließenden elektrischen Stroms ist proportional der elektrischen Spannung." +
                                       "Oder umgekehrt: Ist der als Quotient aus Spannung zu Stromstärke definierte elektrische Widerstand konstant," +
@@ -134,8 +136,11 @@ namespace ETMultiToolKlassenBibliothek
 
                         break;
 
-                    case "2":   //Möglichkeit 2
+
+                    case "2":   //Möglichkeit 2 (Formel)
                         Console.Clear();
+
+                        Console.Write("Ohmsches Gesetz --- Formel\n");
 
                         Console.Write("R = U/I\n");
 
@@ -149,7 +154,10 @@ namespace ETMultiToolKlassenBibliothek
 
                         Console.Write("R = U/I, U = R*I und I = U/R\n\n");
 
+
                         Console.Write("Merkhilfe zum Ohmschen Gesetz:\n\n");
+
+
                         Console.Write("Merdreieck:\n");
 
                         Console.Write("Man zeichnet ein Dreieck mit drei feldern und einem Strich der das Dreieck horizontal halbiert," +
@@ -163,38 +171,40 @@ namespace ETMultiToolKlassenBibliothek
                        
                         break;
 
-                    case "3":   //Möglichkeit 3
+
+                    case "3":   //Möglichkeit 3 (Anwendungsgebiete und Beispiele)
                         Console.Clear();
 
-                        Console.Write("Anwendungsgebiete:\n");
+                        Console.Write("Ohmsches Gesetz --- Anwendungsgebiete\n");
+
                         Console.Write("Eigentlich gilt das ohmsche Gesetz nur für bestimmte Leiter, die sogenannten ohmschen Widerstände." +
                                       "Bei diesen besonderen Widerständen bleibt das Verhältnis aus Spannung und Stromstärke immer konstant.");
-                        Console.Write("Das Ohmsche Gesetz kann zum Beispiel bei Reihenschaltungen, Parallelschaltungen und gemischten Schaltungen zum Einsatz kommen");
+                        Console.Write("Das Ohmsche Gesetz kann zum Beispiel bei Reihenschaltungen, Parallelschaltungen und gemischten Schaltungen zum Einsatz kommen\n");
+
                         Console.Write("Im Alltag wird das Ohmsche Gesetz in der Schule z.B. verwendet um Widerstände, Spannungen und Stromstärken in Schaltungen zu berechnen.");
                         Console.Write("Die meisten technischen Geräte, zum Beispiel Smartphones und Laptops, benötigen zum Betrieb nicht nur eine bestimmte Spannung, sondern auch eine bestimmte Stromstärke. " +
                                       "Mit dem ohmschen Gesetz kann man berechnen, " +
                                       "welche Widerstände man in den Geräten verbauen muss, um auf den passenden Gleichstrom zu kommen. " +
                                       "Außerdem kann man auf ähnlichem Wege herausfinden welchen Widerstand man in einer elektrischen Hitzequelle, " +
                                       "wie einem elektrischen Wasserkocher oder einer Heizung verbauen muss damit man immernoch die gewünschte Temperatur erreicht");
-                        Console.Write("Das ohmsche Gesetz kommt außerdem bei elektronischen Sicherungen zum Einsatz. Diese Bauteile verhindern, " +
+                        Console.Write("Das ohmsche Gesetz kommt auch bei elektronischen Sicherungen zum Einsatz. Diese Bauteile verhindern, " +
                                       "dass die Bauteile innerhalb des Stromkreises bei einem Kurzschluss oder einer Überlastung beschädigt werden. " +
-                                      "Mit dem ohmschen Gesetz kann berechnet werden, was für eine Sicherung für den vorliegenden Stromkreis benötigt wird.");
-                      
+                                      "Mit dem ohmschen Gesetz kann berechnet werden, was für eine Sicherung für den vorliegenden Stromkreis benötigt wird.\n\n");
 
-
-
+                        Console.Write("Beispiele:\n");
 
 
 
 
                         break;
 
-                    case "submenü":   //Möglichkeit 4
+
+                    case "submenü":   //Möglichkeit 4 (Submenü)
                         Console.Clear();
                         submenue = true;   //zurück zum Submenü
                         break;
 
-                    case "exit":   //Möglichkeit 5
+                    case "exit":   //Möglichkeit 5 (exit)
                         Console.Clear();
                         Exit = true;       //Programm schließen
                         break;
@@ -238,27 +248,70 @@ namespace ETMultiToolKlassenBibliothek
                 switch (lAuswahl)
                 {
 
-                    case "1":   //Möglichkeit 1
+                    case "1":   //Möglichkeit 1 (Definition und Bedeutung)
                         Console.Clear();
+
+                        Console.Write("Leistungsgesetz --- Definiton und Bedeutung\n");
+
+                        Console.Write("Die elektrische Leistung gibt an wieviel elektrische Energie in einer gewissen Zeit umgesetzt wird, sie beschreibt den Energieverbrauch.");
+                        Console.Write("Die elektrische Leistung beschreibt auch wieviel elektrische Energie in einer gewissen Zeit in andere Formen von Energie umgewandelt werden.\n");
+
+                        Console.Write("P steht in Rechnungen für die elektrische Leistung.");
+                        Console.Write("Die Einheit der elektrischen Leistung ist Watt (W)");
+                        Console.Write("[P] = 1W\n\n");
+
+
+                        Console.Write("Früher wurde die elektrische Leistung in Pferdestärken(PS) angegeben.");
+                        Console.Write("1PS = 735,5W\n");
+
+                        Console.Write("In der Elektrotechnik treten die elektrische Leistungen Mikrowatt (µW = 1*10-6W), Milliwatt (mW = 1*10⁻³W)," +
+                                      "Kilowatt (kW = 1*10³W) und Megawatt (MW = 1*10⁶W)");
+                        Console.Write("Die verschiedenen Größen entsprechen: 1W = 1000mW, 1mW = 1000µW und 1MW = 1000kW, 1kW = 1000W");
+
 
                         break;
 
-                    case "2":   //Möglichkeit 2
+
+                    case "2":   //Möglichkeit 2 (Formel)
                         Console.Clear();
+
+                        Console.Write("Leistungsgesetz --- Formel\n");
+
+                        Console.Write("Die wichtigste Formel der elektrischen Leistung lautet:\n");
+
+                        Console.Write("P = U/I");
+                        Console.Write("P entspricht der elektrischen Leistung in Watt, U der elektrischen Spannung in Volt und I der elektrischen Stromstärke in Ampere.");
+                        Console.Write("Die elektrische Leistung ist das Produkt aus elektrischer Spannung und elektrischem Strom, das bedeutet das die elektrische Leistung ebenfalls vergrößert wird," +
+                                      "jenachdem wie groß die elektrische Spannung und der elektrische Strom ist.\n");
+
+                        Console.Write("Mithilfe des Ohmschen Gesetzes können weitere Formeln aus dieser hergelitten werden.");
+
+
+
+
 
                         break;
 
-                    case "3":   //Möglichkeit 3
+
+                    case "3":   //Möglichkeit 3 (Anwendungsgebiete und Beispiele)
                         Console.Clear();
+
+                        Console.Write("");
+
+
+
+
+
 
                         break;
 
-                    case "submenü":   //Möglichkeit 4
+
+                    case "submenü":   //Möglichkeit 4 (Submenü)
                         Console.Clear();
                         submenue = true;   //zurück zum Submenü
                         break;
 
-                    case "exit":   //Möglichkeit 5
+                    case "exit":   //Möglichkeit 5 (exit)
                         Console.Clear();
                         Exit = true;       //Programm schließen
                         break;
