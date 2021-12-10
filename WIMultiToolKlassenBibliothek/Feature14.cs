3a318dbc9b9d0726df03acff0698f31981ce9328
Murtaza Behzad <murtaza.b@gso.schule.koeln>
2021-12-10 12:52:47 +0100

    Ersten code Geschrreiben

--- WIMultiToolKlassenBibliothek/Feature14.cs
+++ WIMultiToolKlassenBibliothek/Feature14.cs
@@ -11,7 +11,16 @@ namespace WIMultiToolKlassenBibliothek
         public static void Feature14Funcion1()
         {
             Console.WriteLine("Feature14 SubSubMenue");
-            //Hallo Florian
+            //Hallo Murtaza\Amortisation
+            var inventory = new Dictionary<string, int>()
+             int num;  = 7
+            Console.WriteLine($"Inventory on {DateTime.:30d}");
+            Console.WriteLine("fixed Costs");
+            Console.WriteLine($"|{"Item",7}|{"Quantity",1}|");
+            Console.WriteLine($"[{DateTime.Now,-30:d}] Hour [{DateTime.Now,7:HH}] [{1063.342,15:N2}] feet");
+
+
+
         }
     }
 }
