acc799ddd031b2d535eafa354cf2c28a776cfd43
Jonas Högele <jonas.h50@gso.schule.koeln>
2021-12-14 09:01:21 +0100

    F5 fertig gestellt

--- PHMultiToolKlassenBibliothek/Feature5.cs
+++ PHMultiToolKlassenBibliothek/Feature5.cs
@@ -240,7 +240,10 @@ namespace PHMultiToolKlassenBibliothek
                 Hmenue = false;
 
                 Console.WriteLine("Geben Sie ein, welche Einheit Sie umrechnen wollen:\n");
-                Console.Write("Eingabe:");
+                Console.WriteLine("\t1 - mm in cm");
+                Console.WriteLine("\t2 - cm in dm");
+                Console.WriteLine("\t3 - cm in m");
+                Console.WriteLine("\t4 - m in km");
 
                 HauptAusw = Console.ReadLine();
 
@@ -248,6 +251,26 @@ namespace PHMultiToolKlassenBibliothek
 
                 switch (HauptAusw)
                 {
+                    case "1":
+                        Console.Clear();
+                        SiEinheit.milimeter();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        SiEinheit.dcentimeter();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        SiEinheit.mcentimeter();
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        SiEinheit.meter();
+                        break;
+
                     case "hauptmenü":
                         Console.Clear();
                         Hmenue = true;
@@ -504,7 +527,7 @@ namespace PHMultiToolKlassenBibliothek
                 Console.WriteLine("Willkommen zum Bereich `Umrechnung von Maßeinheiten`. Hier kann man ganz einfach seine Einheiten umrechnen.\n" +
                     "Ganz ohne große Kopfrechnungen oder googlen zu müssen. Wählen Sie ihren gewünschten Bereich und wir rechnen dies für Sie.\n\n" +
                     "Geben Sie nun ihre Einheit Kelvin ein. Andernfalls kannst du `Subexit` eingeben und Sie gelangen sofort zum Physik Menü.\n\n");
-                Console.WriteLine("Geben Sie nun Ihre Volt Kelvin an:");
+                Console.WriteLine("Geben Sie nun Ihre Kelvin an:");
                 string str_Eingabe = Console.ReadLine();
                 
 
@@ -524,6 +547,222 @@ namespace PHMultiToolKlassenBibliothek
                         double Kilometer = doub_Eingabe - 273.15;
                         Console.WriteLine("Wir rechnen ihre eingegebenen Kelvin - 273.15. Ein Beispiel für Sie: 300 Kelvin - 273.15 = 26.25 Grad.");
                         Console.WriteLine($"Somit ergibt sich bei Ihren angegebenen Kelvin: {doub_Eingabe} einen Gradstand von: {Kilometer} Grad.\n");
+                        Console.WriteLine("Wenn Sie fertig sind, drücken Sie eine beliebige Taste, um zurückzukehren.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Errorcode = true;
+                        Console.WriteLine("Fehler, geben Sie eine Zahl größer als 0 ein");
+                        Console.ReadKey();
+                    }
+
+
+                }
+
+
+                Console.Clear();
+
+            }
+            while (Subexit == false & Errorcode == true);
+        }
+        public static void milimeter()
+        {
+            bool Subexit = false;
+            bool Errorcode = false;
+            do
+            {
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Umrechnung Milimeter in Centimeter <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Willkommen zum Bereich `Umrechnung von Maßeinheiten`. Hier kann man ganz einfach seine Einheiten umrechnen.\n" +
+                    "Ganz ohne große Kopfrechnungen oder googlen zu müssen. Wählen Sie ihren gewünschten Bereich und wir rechnen dies für Sie.\n\n" +
+                    "Geben Sie nun ihre Einheit in Milimeter ein. Andernfalls kannst du `Subexit` eingeben und Sie gelangen sofort zum Physik Menü.\n\n");
+                Console.WriteLine("Geben Sie nun Ihre Militmeter an:");
+                string str_Eingabe = Console.ReadLine();
+
+
+
+                if (str_Eingabe == "Subexit")
+                {
+                    Subexit = true;
+                }
+                else
+                {
+                    double doub_Eingabe = Convert.ToDouble(str_Eingabe);
+
+                    Errorcode = false;
+                    if (doub_Eingabe >= 0)
+                    {
+                        Console.WriteLine("Berechnung:\n\n");
+                        double Kilometer = doub_Eingabe / 10;
+                        Console.WriteLine("Wir rechnen ihre eingegebenen Milimeter dividiert durch 10. Ein Beispiel für Sie: 1mm / 10 = 0,1cm.");
+                        Console.WriteLine($"Somit ergibt sich bei Ihren angegebenen Milimeter: {doub_Eingabe} einen Centimeterstand von: {Kilometer} cm.\n");
+                        Console.WriteLine("Wenn Sie fertig sind, drücken Sie eine beliebige Taste, um zurückzukehren.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Errorcode = true;
+                        Console.WriteLine("Fehler, geben Sie eine Zahl größer als 0 ein");
+                        Console.ReadKey();
+                    }
+
+
+                }
+
+
+                Console.Clear();
+
+            }
+            while (Subexit == false & Errorcode == true); // dcentimeter
+        }
+        public static void dcentimeter()
+        {
+            bool Subexit = false;
+            bool Errorcode = false;
+            do
+            {
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Umrechnung Centimeter in Dezimeter <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Willkommen zum Bereich `Umrechnung von Maßeinheiten`. Hier kann man ganz einfach seine Einheiten umrechnen.\n" +
+                    "Ganz ohne große Kopfrechnungen oder googlen zu müssen. Wählen Sie ihren gewünschten Bereich und wir rechnen dies für Sie.\n\n" +
+                    "Geben Sie nun ihre Einheit in Centimeter ein. Andernfalls kannst du `Subexit` eingeben und Sie gelangen sofort zum Physik Menü.\n\n");
+                Console.WriteLine("Geben Sie nun Ihre Centimeter an:");
+                string str_Eingabe = Console.ReadLine();
+
+
+
+                if (str_Eingabe == "Subexit")
+                {
+                    Subexit = true;
+                }
+                else
+                {
+                    double doub_Eingabe = Convert.ToDouble(str_Eingabe);
+
+                    Errorcode = false;
+                    if (doub_Eingabe >= 0)
+                    {
+                        Console.WriteLine("Berechnung:\n\n");
+                        double Kilometer = doub_Eingabe / 10;
+                        Console.WriteLine("Wir rechnen ihre eingegebenen Centimeter dividiert durch 10. Ein Beispiel für Sie: 1cm / 10 = 0,1dm.");
+                        Console.WriteLine($"Somit ergibt sich bei Ihren angegebenen Centimeter: {doub_Eingabe} einen Dezimeterstand von: {Kilometer} dm.\n");
+                        Console.WriteLine("Wenn Sie fertig sind, drücken Sie eine beliebige Taste, um zurückzukehren.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Errorcode = true;
+                        Console.WriteLine("Fehler, geben Sie eine Zahl größer als 0 ein");
+                        Console.ReadKey();
+                    }
+
+
+                }
+
+
+                Console.Clear();
+
+            }
+            while (Subexit == false & Errorcode == true); 
+        }
+        public static void mcentimeter()
+        {
+            bool Subexit = false;
+            bool Errorcode = false;
+            do
+            {
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Umrechnung Centimeter in Meter <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Willkommen zum Bereich `Umrechnung von Maßeinheiten`. Hier kann man ganz einfach seine Einheiten umrechnen.\n" +
+                    "Ganz ohne große Kopfrechnungen oder googlen zu müssen. Wählen Sie ihren gewünschten Bereich und wir rechnen dies für Sie.\n\n" +
+                    "Geben Sie nun ihre Einheit in Centimeter ein. Andernfalls kannst du `Subexit` eingeben und Sie gelangen sofort zum Physik Menü.\n\n");
+                Console.WriteLine("Geben Sie nun Ihre Centimeter an:");
+                string str_Eingabe = Console.ReadLine();
+
+
+
+                if (str_Eingabe == "Subexit")
+                {
+                    Subexit = true;
+                }
+                else
+                {
+                    double doub_Eingabe = Convert.ToDouble(str_Eingabe);
+
+                    Errorcode = false;
+                    if (doub_Eingabe >= 0)
+                    {
+                        Console.WriteLine("Berechnung:\n\n");
+                        double Kilometer = doub_Eingabe / 100;
+                        Console.WriteLine("Wir rechnen ihre eingegebenen Meter dividiert durch 100. Ein Beispiel für Sie: 1cm / 100 = 0,01m.");
+                        Console.WriteLine($"Somit ergibt sich bei Ihren angegebenen Centimeter: {doub_Eingabe} einen Meterstand von: {Kilometer} m.\n");
+                        Console.WriteLine("Wenn Sie fertig sind, drücken Sie eine beliebige Taste, um zurückzukehren.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Errorcode = true;
+                        Console.WriteLine("Fehler, geben Sie eine Zahl größer als 0 ein");
+                        Console.ReadKey();
+                    }
+
+
+                }
+
+
+                Console.Clear();
+
+            }
+            while (Subexit == false & Errorcode == true);
+        }
+        public static void meter()
+        {
+            bool Subexit = false;
+            bool Errorcode = false;
+            do
+            {
+                Console.WriteLine
+                    (FiggleFonts.Slant.Render("BFTMultiTool"));
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Umrechnung Meter in Kilometer <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine("Willkommen zum Bereich `Umrechnung von Maßeinheiten`. Hier kann man ganz einfach seine Einheiten umrechnen.\n" +
+                    "Ganz ohne große Kopfrechnungen oder googlen zu müssen. Wählen Sie ihren gewünschten Bereich und wir rechnen dies für Sie.\n\n" +
+                    "Geben Sie nun ihre Einheit in Meter ein. Andernfalls kannst du `Subexit` eingeben und Sie gelangen sofort zum Physik Menü.\n\n");
+                Console.WriteLine("Geben Sie nun Ihre Meter an:");
+                string str_Eingabe = Console.ReadLine();
+
+
+
+                if (str_Eingabe == "Subexit")
+                {
+                    Subexit = true;
+                }
+                else
+                {
+                    double doub_Eingabe = Convert.ToDouble(str_Eingabe);
+
+                    Errorcode = false;
+                    if (doub_Eingabe >= 0)
+                    {
+                        Console.WriteLine("Berechnung:\n\n");
+                        double Kilometer = doub_Eingabe / 1000;
+                        Console.WriteLine("Wir rechnen ihre eingegebenen Meter dividiert durch 1000. Ein Beispiel für Sie: 1m / 1000 = 0,001m.");
+                        Console.WriteLine($"Somit ergibt sich bei Ihren angegebenen Meter: {doub_Eingabe} einen Kilometerstand von: {Kilometer} km.\n");
                         Console.WriteLine("Wenn Sie fertig sind, drücken Sie eine beliebige Taste, um zurückzukehren.");
                         Console.ReadKey();
                     }
