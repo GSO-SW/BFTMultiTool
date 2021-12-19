1a72d56fd3433fbdd7bb27dce0e3583e97d76b5c
Sergey Zavadovskiy <sergey.z@gso.schule.koeln>
2021-12-19 15:46:34 +0100

    Ertser Entwurf

--- ITMultiToolKlassenBibliothek/Feature21.cs
+++ ITMultiToolKlassenBibliothek/Feature21.cs
@@ -10,8 +10,50 @@ namespace ITMultiToolKlassenBibliothek
     {
         public static void Feature21Funcion1()
         {
-            Console.WriteLine("Feature21 SubSubMenue");
 
+            do
+            {
+                Console.Title = "Bandbreitenrechner";
+
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Bandbreitenrechner <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+                string strds = null;
+                try
+                {
+                    
+                    Console.Write("Geben sie ihre Downloadgröße in MB ein:");
+                    strds = Console.ReadLine();
+                    int ds = Convert.ToInt32(strds);
+                    Console.Write("Geben sie ihre Internetgeschwindigkeit in Mbit ein:");
+                    strds = Console.ReadLine();
+                    int igs = Convert.ToInt32(strds);
+                    int tsec = ds * 8 / igs;
+                    int std = tsec / 3600;
+                    int rsec = tsec % 3600;
+                    int min = rsec / 60;
+                    int sec = rsec % 60;
+                    Console.WriteLine("Ihr Donwload wird {0}h {1}m {2}s  dauern", std, min, sec);
+                    Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    if (strds == "exit")
+                        Environment.Exit(0);
+                    if (strds == "hauptmenü")
+                        return;
+                    Console.WriteLine("Falsche Eingabe Geben sie eine belibige Taste ein um wieder anzufangen");
+                    Console.ReadKey();
+                }
+                finally
+                {
+                    Console.Clear();
+                }
+
+
+
+
+            } while (true);
         }
 
     }
